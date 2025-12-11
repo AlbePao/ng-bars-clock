c421bc782b234c120ad79766a0be6f81140d2d70
Alberto Paolucci <apaolucci@softstrategy.it>
2025-12-11 17:50:18 +0100

    code cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideBrowserGlobalErrorListeners()],
+}).catch((err) => console.error(err));
