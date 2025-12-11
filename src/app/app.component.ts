66b445a6713814321fd2eaf2f62251dcc8865dfa
Alberto Paolucci <apaolucci@softstrategy.it>
2025-12-11 12:05:01 +0100

    clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, OnInit, signal } from '@angular/core';
 import { NumbersBarComponent } from './numbers-bar';
 
 @Component({
@@ -6,5 +6,27 @@ import { NumbersBarComponent } from './numbers-bar';
   imports: [NumbersBarComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-full h-full bg-stone-400 block overflow-hidden',
+  },
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  currentTime = signal(this.getCurrentTime());
+  currentHours = computed(() => this.currentTime()[0]);
+  currentMinutes = computed(() => this.currentTime()[1]);
+  currentSeconds = computed(() => this.currentTime()[2]);
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentTime.set(this.getCurrentTime());
+    }, 1000);
+  }
+
+  getCurrentTime(): string[] {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return [hours, minutes, seconds];
+  }
+}
